4b97d93207127854a27dda34e4de255b91f29f16
Nicola <nicola.marano02@gmail.com>
2024-03-25 12:39:32 +0100

    lettura categorie e classe delle categorie dal json

--- JS/Categories.js
+++ JS/Categories.js
@@ -1,220 +1,15 @@
-const categories = {
-  Matematica: [
-    {
-      name: "Esercizio Trifase",
-      link: "Siti/Matematica/Esercizio Trifase/index.html",
-    },
-    {
-      name: "Biografia su Alan Turing",
-      link: "Siti/Matematica/Biografia su Alan Turing/index.html",
-    },
-    {
-      name: "Calcolatrice",
-      link: "Siti/Matematica/Calcolatrice/index.html",
-    },
-    {
-      name: "Conversioni Base",
-      link: "Siti/Matematica/Conversione Basi/index.html",
-    },
-    {
-      name: "Verifica Numero Primo",
-      link: "Siti/Matematica/Numero Primo/index.html",
-    },
-    {
-      name: "Verifica Numeri Primi Precedenti",
-      link: "Siti/Matematica/Verifica Numeri Primi Precedenti/index.html",
-    },
-    { name: "Tabelline", link: "Siti/Matematica/Tabelline/index.html" },
-    {
-      name: "Triangolo di Tartaglia",
-      link: "Siti/Matematica/Triangolo di Tartaglia/index.html",
-    },
-    {
-      name: "Calcola Esponenziali / Radici",
-      link: "Siti/Matematica/Esponenziali  e Radici/index.html",
-    },
-    {
-      name: "Calcola Logaritmi",
-      link: "Siti/Matematica/Logaritmi/index.html",
-    },
-    { name: "Fattoriale", link: "Siti/Matematica/Fattoriale/index.html" },
-    {
-      name: "Calcola Numero di Fibonacci",
-      link: "Siti/Matematica/Fibonacci/index.html",
-    },
-    {
-      name: "Calcolatore Triangolo di Pitagora",
-      link: "Siti/Matematica/Pitagora/index.html",
-    },
-    { name: "Gestione Punteggi", link: "Siti/Matematica/Punteggi/index.html" },
-    {
-      name: "Calcolo Segno Zodiacale",
-      link: "Siti/Matematica/Segno_Zodiacale/index.html",
-    },
-    {
-      name: "Stampa Numeri Pari / Dispari o Tutti",
-      link: "Siti/Matematica/StampaNumeri/index.html",
-    },
-    {
-      name: "Calcolo Valore Assoluto",
-      link: "Siti/Matematica/Valore_Assoluto/index.html",
-    },
-    { name: "Ahmes", link: "Siti/Matematica/Ahmes/index.html" },
-    { name: "Euclide", link: "Siti/Matematica/Euclide/index.html" },
-    { name: "Collatz", link: "Siti/Matematica/Collatz/index.html" },
-    {
-      name: "Calcolo Trigonometrico",
-      link: "Siti/Matematica/Trigonometria/index.html",
-    },
-  ],
-  Bici: [{ name: "Giri In Bici", link: "https://giri-in-bici.netlify.app/" }],
-  Random: [
-    { name: "Generatore Password", link: "Siti/Random/Password/index.html" },
-    { name: "Lancio dei dadi", link: "Siti/Random/Lancio_Dadi/index.html" },
-    {
-      name: "Lancio dei dadi con n facce Grafico",
-      link: "Siti/Random/Lancio Dadi N facce/index.html",
-    },
-    {
-      name: "Lancio dei dadi con n facce Normale",
-      link: "Siti/Random/Lancio_dei_Dadi_N/index.html",
-    },
-    {
-      name: "Estrazione Lettera Casuale",
-      link: "Siti/Random/Lettera_Casuale/index.html",
-    },
-    {
-      name: "Estrazione Numeri Casuali tra un intervallo",
-      link: "Siti/Random/Numeri_Casuali/index.html",
-    },
-    {
-      name: "Estrazionne Lancio n Monete",
-      link: "Siti/Random/Lancio Monete/index.html",
-    },
-    {
-      name: "Generatore di Carte (Scala 40 o Briscola)",
-      link: "Siti/Random/Generatore di Carte/index.html",
-    },
-    {
-      name: "Criptazione con Alfabeto Opposto",
-      link: "Siti/Random/Criptazione Alfabeto Contrario/index.html",
-    },
-    {
-      name: "Genera Nomi Casuali",
-      link: "Siti/Random/Nomi Casuali/index.html",
-    },
-    {
-      name: "Coach Life Generator",
-      link: "Siti/Random/Coach_Life_Genertor/index.html",
-    },
-    {
-      name: "Generatore Colore Casuale",
-      link: "Siti/Random/Colore_Casuale/index.html",
-    },
-  ],
-  Giochi: [
-    { name: "Gioco della Tombola", link: "Siti/Giochi/Tombola/index.html" },
-    {
-      name: "Gioco Sasso Carta Forbice",
-      link: "Siti/Giochi/Sasso_Carta_Forbice/index.html",
-    },
-    { name: "Gioco del Tris", link: "Siti/Giochi/Tris/index.html" },
-    { name: "Forza 4", link: "Siti/Giochi/Forza 4/index.html" },
-    { name: "Snake Game", link: "Siti/Giochi/SnakeGame/index.html" },
-    {
-      name: "Indovinello dei Dinosauri",
-      link: "Siti/Giochi/Dino-Game/Index.html",
-    },
-    {
-      name: "Generazione 4 Lettere Casuali",
-      link: "Siti/Giochi/Generated_4_Letters/index.html",
-    },
-    {
-      name: "Slot Macchine",
-      link: "Siti/Giochi/Slot_Macchine/index.html",
-    },
-    {
-      name: "Generazione 4 Numeri Casuali",
-      link: "Siti/Giochi/Generated_4_Numbers/index.html",
-    },
-    {
-      name: "Rotella Movimentata",
-      link: "Siti/Giochi/Rotella Movimentata/index.html",
-    },
-  ],
-  Borsa: [
-    { name: "Cambi Valute", link: "Siti/Borsa/Cambi Valute/index.html" },
-    {
-      name: "Valore Titoli di Borsa",
-      link: "Siti/Borsa/Valore Titoli/index.html",
-    },
-  ],
-  Calendario: [
-    {
-      name: "Calcola Giorno della Settimana",
-      link: "Siti/Calendario/Controllo_data/index.html",
-    },
-    {
-      name: "Cronometro / Timer",
-      link: "Siti/Calendario/Cronometro_timer/index.html",
-    },
-    { name: "Calendario", link: "Siti/Calendario/Calendario/index.html" },
-    {
-      name: "Calcolatore di Tempo",
-      link: "Siti/Calendario/Calcolatore_di_tempo/index.html",
-    },
-  ],
-  Info_Paesi_Stati: [
-    {
-      name: "Verifica Fuso Orario Città",
-      link: "Siti/Info_Paesi_Stati/Fuso Orario/index.html",
-    },
-    { name: "Info Stato", link: "SIti/Info_Paesi_Stati/Stati_info/index.html" },
-  ],
-  Natale: [
-    {
-      name: "Calendario dell'Avvento",
-      link: "Siti/Natale/Advent-Calendar/index.html",
-    },
-    {
-      name: "Conto alla rovescia data di Natale",
-      link: "Siti/Natale/Countdown_Natale/index.html",
-    },
-    {
-      name: "Conto Alla rovescia Anno Nuovo",
-      link: "Siti/Natale/Countdown_anno_nuovo/index.html",
-    },
-  ],
-  Salute: [
-    {
-      name: "Sito che Calcola Altezza ideale o Peso Ideale",
-      link: "SIti/Salute/Calcolo_peso_altezza_ideale/index.html",
-    },
-  ],
-  Pasqua: [
-    {
-      name: "Calcola la Data di Pasqua",
-      link: "Siti/Pasqua/Pasqua/index.html",
-    },
-    {
-      name: "Conto alla rovescia data di Pasqua",
-      link: "Siti/Pasqua/Countdown_Pasqua/index.html",
-    },
-  ],
-},categoryClasses = {
-  Bici: "contenitore1",
-  Salute: "contenitore1",
-  Borsa: "contenitore1",
-  Info_Paesi_Stati: "contenitore1",
-  Pasqua: "contenitore1",
-  Calendario: "contenitore1",
-  Matematica: "contenitore3",
-  Random: "contenitore2",
-  Giochi: "contenitore2",
-  Natale: "contenitore1",
-};
+async function loadCategoriesFromJSON() {
+  try {
+    const response = await fetch("JS/Categories.json"),
+      data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Errore durante il caricamento dei dati JSON:", error);
+    return null;
+  }
+}
 
-function generateCategoryHTML(categoryName, category) {
+function generateCategoryHTML(categoryName, category, categoryClasses) {
   const containerClass = categoryClasses[categoryName],
     linksHTML = category
       .map(
@@ -225,12 +20,20 @@ function generateCategoryHTML(categoryName, category) {
   return `<h2>${categoryName}</h2><br><div class="container">${linksHTML}</div>`;
 }
 
-function displayCategory(categoryName) {
-  const category = categories[categoryName];
-  if (category) {
-    document.getElementById("dati").innerHTML = generateCategoryHTML(
-      categoryName,
-      category
-    );
+async function displayCategory(categoryName) {
+  const data = await loadCategoriesFromJSON();
+  if (data) {
+    const categories = data.categories,
+      categoryClasses = data.categoryClasses,
+      category = categories[categoryName];
+    if (category) {
+      document.getElementById("dati").innerHTML = generateCategoryHTML(
+        categoryName,
+        category,
+        categoryClasses
+      );
+    } else {
+      console.error(`La categoria '${categoryName}' non è stata trovata.`);
+    }
   }
 }
