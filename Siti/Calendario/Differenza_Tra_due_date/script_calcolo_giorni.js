f7fc34de315b4392c0aa1ead3200541affa486f1
Nicola <nicola.marano02@gmail.com>
2024-03-26 12:14:10 +0100

    modifca codice differenza tra date

--- Siti/Calendario/Differenza_Tra_due_date/script_calcolo_giorni.js
+++ Siti/Calendario/Differenza_Tra_due_date/script_calcolo_giorni.js
@@ -1,34 +1,34 @@
 function calcolaDifferenzaTempo() {
   let data1 = new Date(document.getElementById("inputDate1").value),
-      data2 = new Date(document.getElementById("inputDate2").value);
+    data2 = new Date(document.getElementById("inputDate2").value);
 
   if (isNaN(data1) || isNaN(data2))
-      document.getElementById("risultato").innerHTML = "Inserisci date valide.";
+    document.getElementById("risultato").innerHTML = "Inserisci date valide.";
   else {
-      let differenza = Math.abs(data2 - data1),
-          anni = Math.floor(differenza / (1000 * 60 * 60 * 24 * 365.25)),
-          giorni = Math.floor(
-              (differenza % (1000 * 60 * 60 * 24 * 365.25)) / (1000 * 60 * 60 * 24)
-          ),
-          ore = Math.floor((differenza % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-          minuti = Math.floor((differenza % (1000 * 60 * 60)) / (1000 * 60)),
-          secondi = Math.floor((differenza % (1000 * 60)) / 1000),
-          risultato = `La differenza è di `;
+    let differenza = Math.abs(data2 - data1),
+      millisecondiInUnAnno = 1000 * 60 * 60 * 24 * 365.25,
+      anni = Math.floor(differenza / millisecondiInUnAnno),
+      giorniRimasti = differenza % millisecondiInUnAnno,
+      millisecondiInUnGiorno = 1000 * 60 * 60 * 24,
+      giorni = Math.floor(giorniRimasti / millisecondiInUnGiorno),
+      oreRimanenti = giorniRimasti % millisecondiInUnGiorno,
+      millisecondiInUnOra = 1000 * 60 * 60,
+      ore = Math.floor(oreRimanenti / millisecondiInUnOra),
+      minutiRimasti = oreRimanenti % millisecondiInUnOra,
+      millisecondiInUnMinuto = 1000 * 60,
+      minuti = Math.floor(minutiRimasti / millisecondiInUnMinuto),
+      secondi = Math.floor((minutiRimasti % millisecondiInUnMinuto) / 1000),
+      risultato = `La differenza è di `;
 
-      if (anni > 0)
-          risultato += `${anni} anni, `;
-      if (giorni > 0)
-          risultato += `${giorni} giorni, `;
-      if (ore > 0)
-          risultato += `${ore} ore, `;
-      if (minuti > 0)
-          risultato += `${minuti} minuti, `;
-      if (secondi > 0)
-          risultato += `${secondi} secondi, `;
+    if (anni > 0) risultato += `${anni} anni, `;
+    if (giorni > 0) risultato += `${giorni} giorni, `;
+    if (ore > 0) risultato += `${ore} ore, `;
+    if (minuti > 0) risultato += `${minuti} minuti, `;
+    if (secondi > 0) risultato += `${secondi} secondi, `;
 
-      risultato = risultato.slice(0, -2);
-      risultato += ".";
+    risultato = risultato.slice(0, -2);
+    risultato += ".";
 
-      document.getElementById("risultato").innerHTML = risultato;
+    document.getElementById("risultato").innerHTML = risultato;
   }
 }
