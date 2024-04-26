fb498228f6aa9a35d9cd7baf69963176d2bc0d23
Nicola <nicola.marano02@gmail.com>
2024-04-26 07:45:39 +0200

    gggiiunta nome dinamica nella generazione nomi casuali

--- Site/Random/Nomi Casuali/scriptname.js
+++ Site/Random/Nomi Casuali/scriptname.js
@@ -4,8 +4,9 @@ function aggiungiNome() {
   let inputNome = document.getElementById("nomeInput").value;
   if (inputNome.trim() !== "") {
     nomi.push(inputNome);
-    document.getElementById("nomiInseriti").textContent =
-      `Nomi inseriti: ${nomi.join(", ")}`;
+    document.getElementById(
+      "nomiInseriti"
+    ).textContent = `Nomi inseriti: ${nomi.join(", ")}`;
     document.getElementById("nomeInput").value = ""; // Pulisce l'input
   }
 }
@@ -13,7 +14,19 @@ function aggiungiNome() {
 function generaNomeCasuale() {
   if (nomi.length > 0) {
     let nomeCasuale = nomi[Math.floor(Math.random() * nomi.length)];
-    document.getElementById("nomeCasuale").textContent =
-      `Nome Casuale: ${nomeCasuale}`;
+    document.getElementById(
+      "nomeCasuale"
+    ).textContent = `Nome Casuale: ${nomeCasuale}`;
   }
 }
+
+document
+  .getElementById("generateButton")
+  .addEventListener("click", function () {
+    const randomGenerator = setInterval(generaNomeCasuale, 150);
+
+    setTimeout(() => {
+      clearInterval(randomGenerator);
+      generaNomeCasuale();
+    }, 500);
+  });
