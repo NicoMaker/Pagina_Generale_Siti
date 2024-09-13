1bda5d406ba5abb64b8862c03e2c807cff6dde31
NicoMaker <Nicola.marano02gmail.com>
2024-09-13 09:28:16 +0200

    ottimizatore genrratore carte + alri css

--- Site/Random/Generatore_di_Carte/Script.js
+++ Site/Random/Generatore_di_Carte/Script.js
@@ -1,41 +1,57 @@
 let suitImages = {},
   cardValues = {};
 
-fetch("cardsData.json")
-  .then((response) => response.json())
-  .then((data) => {
-    suitImages = data.suitImages;
-    cardValues = data.cardValues;
-  })
-  .catch((error) => console.error("Error fetching JSON:", error));
+const initialize = () =>
+    document
+      .getElementById("generateButton")
+      .addEventListener("click", startCardAnimation),
+  displayCard = (card) =>
+    (document.getElementById(
+      "card"
+    ).innerHTML = `<img src="${card.image}" alt="${card.suit}"> <br> ${card.value}`),
+  isValidGameType = (gameType) => Object.keys(cardValues).includes(gameType),
+  getRandomCard = (gameType) => {
+    return {
+      suit: (suits = Object.keys(cardValues[gameType]))[
+        Math.floor(Math.random() * suits.length)
+      ],
+      value: (values =
+        cardValues[gameType][suits[Math.floor(Math.random() * suits.length)]])[
+        Math.floor(Math.random() * values.length)
+      ],
+      image: suitImages[suits[Math.floor(Math.random() * suits.length)]],
+    };
+  };
+
+function fetchData() {
+  return fetch("cardsData.json")
+    .then((response) => response.json())
+    .then((data) => {
+      suitImages = data.suitImages;
+      cardValues = data.cardValues;
+    })
+    .catch((error) => console.error("Error fetching JSON:", error));
+}
 
 function generateCard() {
   const gameType = document.getElementById("game-type").value;
 
-  if (!["briscola", "scala-40", "tutti"].includes(gameType)) {
+  if (!isValidGameType(gameType)) {
     alert("Please select a valid game type.");
     return;
   }
 
-  const suits = Object.keys(cardValues[gameType]),
-    randomSuitIndex = Math.floor(Math.random() * suits.length),
-    suit = suits[randomSuitIndex],
-    values = cardValues[gameType][suit],
-    randomValueIndex = Math.floor(Math.random() * values.length),
-    value = values[randomValueIndex],
-    cardImage = suitImages[suit],
-    cardElement = document.getElementById("card");
-
-  cardElement.innerHTML = `<img src="${cardImage}" alt="${suit}"> <br> ${value}`;
+  const card = getRandomCard(gameType);
+  displayCard(card);
 }
 
-document
-  .getElementById("generateButton")
-  .addEventListener("click", function () {
-    const randomGenerator = setInterval(generateCard, 150);
+function startCardAnimation() {
+  const randomGenerator = setInterval(generateCard, 150);
 
-    setTimeout(() => {
-      clearInterval(randomGenerator);
-      generateCard();
-    }, 500);
-  });
+  setTimeout(() => {
+    clearInterval(randomGenerator);
+    generateCard();
+  }, 500);
+}
+
+fetchData().then(initialize);
