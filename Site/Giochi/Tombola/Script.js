522898a6e9acbca15186f4b0ad6df1135ace1169
Nicola <93321282+NicoMaker@users.noreply.github.com>
2025-01-04 15:00:07 +0100

    Update Script.js

--- Site/Giochi/Tombola/Script.js
+++ Site/Giochi/Tombola/Script.js
@@ -74,15 +74,14 @@ function tombola_writeln() {
   const container = document.getElementById("tombola-container");
   let tableContent = '';
 
-  for (let block = 0; block < 6; block++) { // 6 tabelle
+  for (let block = 0; block < 6; block++) { // 6 blocchi (cartelle)
     tableContent += '<div class="sub-table"><table>';
 
-    for (let row = 0; row < 3; row++) { // Ogni tabella ha 3 righe
+    for (let row = 0; row < 3; row++) { // Ogni cartella ha 3 righe
       tableContent += '<tr>';
 
       for (let col = 0; col < 5; col++) { // Ogni riga ha 5 numeri
-        // Calcola il numero corrente basato su blocco, riga e colonna
-        let number = block * 5 + row * 10 + col + 1; 
+        let number = block * 15 + row * 5 + col + 1; // Calcola il numero corrente
         tableContent += `<td id="nr${number}" onclick="choseMe(this)">${number}</td>`;
       }
 
