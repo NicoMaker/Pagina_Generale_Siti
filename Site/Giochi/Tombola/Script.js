58ba3cca6bc06e185aad64576f6f0f63ce7d80d0
NicoMaker <nicola.marano02@gmail.com>
2025-03-22 10:17:09 +0100

    Add speech synthesis for game start and reset functionality in Tombola

--- Site/Giochi/Tombola/Script.js
+++ Site/Giochi/Tombola/Script.js
@@ -1,8 +1,13 @@
 let orig = [],
-  hist = new Set();
+  hist = new Set(),
+  firstStart = true, // Controlla se è la prima volta che si preme il bottone
+  gameStarted = false; // Indica se il gioco è in corso
 
 function init() {
+  orig = [];
   for (let ii = 1; ii <= 90; ii++) orig.push(ii);
+  // Annuncia "Si inizia!" all'avvio senza estrarre numeri
+  speechSynthesis.speak(new SpeechSynthesisUtterance("Si inizia!"));
 }
 
 function BlinkNode(celnode, times, millis) {
@@ -20,9 +25,18 @@ function BlinkNode(celnode, times, millis) {
   };
 }
 
-function announceNumber(nn) {
-  const utterance = new SpeechSynthesisUtterance(`È uscito il numero ${nn}`);
-  speechSynthesis.speak(utterance);
+const announceNumber = (nn) =>
+  speechSynthesis.speak(new SpeechSynthesisUtterance(`${nn}`));
+
+function startGame() {
+  if (firstStart) {
+    // Al primo click dice solo "Si inizia!" senza estrarre numeri
+    speechSynthesis.speak(new SpeechSynthesisUtterance("Si inizia!"));
+    firstStart = false;
+    gameStarted = true;
+    return;
+  }
+  extractRandom();
 }
 
 function selectNr(nn) {
@@ -60,9 +74,18 @@ function extractRandom() {
   if (!hist.has(nn)) selectNr(nn);
 }
 
+function resetGame() {
+  firstStart = true; // Ripristina l'annuncio "Si inizia!"
+  gameStarted = false;
+  hist.clear();
+  init();
+  document.querySelectorAll("td").forEach((td) => (td.className = ""));
+  dumpHist();
+}
+
 async function fetchTables() {
-  const response = await fetch("../tables.json");
-  const data = await response.json();
+  const response = await fetch("../tables.json"),
+    data = await response.json();
   return data.tables;
 }
 
