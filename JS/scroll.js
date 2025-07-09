731e16bfc9170005ad1f758a5dfd0194df73e0e7
NicoMaker <nicola.marano02@gmail.com>
2025-07-09 14:27:55 +0200

    scroll

---
+++ JS/scroll.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section[id]");
+
+    const updateURLOnScroll = () => {
+        let scrollY = window.scrollY + 120; // aggiusta se il tuo header è fisso
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.offsetHeight;
+            const sectionId = section.getAttribute("id");
+
+            if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+                history.replaceState(null, null, `#${sectionId}`);
+            }
+        });
+    };
+
+    window.addEventListener("scroll", updateURLOnScroll);
+});
